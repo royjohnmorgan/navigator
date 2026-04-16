eea1f9a9bfe9ff1bcc630c2e0ed8b2f3a48fd25b
royjohnmorgan <royjohnmorgan@users.noreply.github.com>
2026-04-16 13:34:09 +0100

    first commit, shell project

---
+++ Core/Inc/run.h
@@ -0,0 +1,9 @@
+#ifndef INC_RUN_H_
+#define INC_RUN_H_
+
+
+#include "main.h"
+
+int run(SPI_HandleTypeDef *spi);
+
+#endif
---
+++ Core/Src/run.c
@@ -0,0 +1,13 @@
+#include "run.h"
+
+int run(SPI_HandleTypeDef *spi)
+{
+
+
+  while (1){
+		HAL_Delay(2000);
+  }
+
+
+  return 0;
+}
